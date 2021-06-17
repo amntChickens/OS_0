aa7748ed0fe34aa5327bbee50534849bca7ea8ad
amntChickens <67888548+amntChickens@users.noreply.github.com>
2021-06-17 10:21:30 -0400

    Update bootloader.asm

--- src/boot loader/bootloader.asm
+++ src/boot loader/bootloader.asm
@@ -1,6 +1,11 @@
 start: jmp boot
 
-
+GDT_ADDR:
+dq 0x0000000000000000 ; null descriptor
+; kernel-code , kernel-data , user-code , user-data...
+GDT_SPECS_ADDR:
+dd GDT_ADDR                   ; pointer
+dw GDT_SPECS_ADDR - GDT_ADDR  ; size
 
 
 boot:
@@ -8,10 +13,13 @@ boot:
   cli
   cld
   
-  ; set up segmentation
-  lgdt [GDT_SPECS_ADDR]
   
-  mov es, 0x80
-  mov
+  ; set up segmentation
+  lgdt [GDT_SPECS_ADDR] ; load table
+  
+  mov es , 0x80 ; set registers
+  jmp 0x10:change_cs 
+  change_cs:
+  
   
   ; 
